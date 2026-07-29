46b80101f50e2fbc49a6c9f542ba50be86da48fc
lookjota <lookjota@gmail.com>
2026-07-29 20:20:57 -0300

    feat: initialize Amigo do Lar v2 on Logos Page Engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { AppNavigation } from './apps/instituto-logos/components/AppNavigation'
-import { navigationItems } from './apps/instituto-logos/config/navigation'
-import { routes } from './apps/instituto-logos/config/routes'
-import { NotFoundPage } from './apps/instituto-logos/pages/NotFoundPage'
-import { PageRoute } from './apps/instituto-logos/pages/PageRoute'
-import { pageSectionRegistry } from './apps/instituto-logos/registry/pageSectionRegistry'
-import { findNavigationIssues } from './domain/navigation/navigationResolver'
-import {
-  findRouteIssues,
-  findRouteNavigationIssues,
-} from './domain/navigation/routeResolver'
-import { PageSectionRegistryProvider } from './engine/PageSectionRegistry'
-
-const navigationIssues = findNavigationIssues(navigationItems)
-const routeIssues = findRouteIssues(routes)
-const routeNavigationIssues = findRouteNavigationIssues(
-  routes,
-  navigationItems,
-)
-
-if (
-  navigationIssues.length > 0 ||
-  routeIssues.length > 0 ||
-  routeNavigationIssues.length > 0
-) {
-  throw new Error(
-    `Invalid application configuration: ${JSON.stringify({
-      navigation: navigationIssues,
-      routes: routeIssues,
-      routeNavigation: routeNavigationIssues,
-    })}`,
-  )
-}
+import { AmigoDoLarApp } from './apps/amigo-do-lar/AmigoDoLarApp'
 
 function App() {
-  return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <PageSectionRegistryProvider value={pageSectionRegistry}>
-        <AppNavigation items={navigationItems} />
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<PageRoute pageSlug={route.pageSlug} />}
-            />
-          ))}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </PageSectionRegistryProvider>
-    </BrowserRouter>
-  )
+  return <AmigoDoLarApp />
 }
 
 export default App
---
+++ src/apps/amigo-do-lar/AmigoDoLarApp.tsx
@@ -0,0 +1,8 @@
+export function AmigoDoLarApp() {
+  return (
+    <main>
+      <h1>Amigo do Lar v2</h1>
+      <p>Construído sobre o Logos Page Engine.</p>
+    </main>
+  )
+}
